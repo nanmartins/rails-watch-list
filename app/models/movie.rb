1542f6334b03fd3ec873253f519e360c818551a8
Renan Martins <renanmartins989@gmail.com>
2022-06-23 09:01:00 +0100

    few changes on MVC

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,8 +1,6 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
 
-  validates :title, uniqueness: true
-  validates :title, presence: true
-  validates :overview, uniqueness: true
+  validates :title, presence: true, uniqueness: true
   validates :overview, presence: true
 end
